1bb0031fe6e0548017043f8eec3ec86c96824d3a
joaopedroa.6 <joaopedroa.6@hotmail.com>
2018-10-02 20:44:03 -0300

    Ajuste

--- src/main/java/br/com/projetospring/controllers/ExcelController.java
+++ src/main/java/br/com/projetospring/controllers/ExcelController.java
@@ -42,21 +42,21 @@ public class ExcelController {
 		try {
 			List<UserInfo> lstUser = new ArrayList<>();
 			int i = 0;
-			// Creates a workbook object from the uploaded excelfile
+			
 			XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
-			// Creates a worksheet object representing the first sheet
+			
 			XSSFSheet worksheet = workbook.getSheetAt(0);
-			// Reads the data in excel file until last row is encountered
+			
 			while (i <= worksheet.getLastRowNum()) {
-				// Creates an object for the UserInfo Model
+				
 				UserInfo user = new UserInfo();
-				// Creates an object representing a single row in excel
+				
 				XSSFRow row = worksheet.getRow(i++);
-				// Sets the Read data to the model class
+				
 				user.setId((int) row.getCell(0).getNumericCellValue());
 				user.setUsername(row.getCell(1).getStringCellValue());
 				user.setInputDate(row.getCell(2).getDateCellValue());
-				// persist data into database in here
+				
 				lstUser.add(user);
 			}
 			System.out.println(lstUser);
